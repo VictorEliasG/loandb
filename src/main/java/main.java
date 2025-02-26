7b4122916fd01f61b7fb275f0d9122fd59609a75
VictorEliasG <49217180+VictorEliasG@users.noreply.github.com>
2025-02-26 15:05:39 -0600

    First Commit
     Javalin Working

---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+
+import io.javalin.Javalin;
+
+// Loan Management System - RESTful back-end application using Java 17, JDBC, and Javalin
+
+public class main {
+    public static void main(String[] args) {
+        var app = Javalin.create(/*config*/)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(7070);
+    }
+}
